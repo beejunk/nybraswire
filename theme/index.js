1827d529641f0ea35aa57b6c543e9bbf000beb8e
Brian David <beejunk@gmail.com>
2019-07-02 23:10:40 -0500

    Add bash and ini to language highlighting.

--- theme/index.js
+++ theme/index.js
@@ -1,10 +1,14 @@
 import hljs from 'highlight.js/lib/highlight';
 import javascript from 'highlight.js/lib/languages/javascript';
+import bash from 'highlight.js/lib/languages/bash';
+import ini from 'highlight.js/lib/languages/ini';
 import 'highlight.js/styles/solarized-dark.css';
 import { createContext } from 'react';
 import theme from './nybraswire';
 
 hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('ini', ini);
 
 const ThemeContext = createContext(theme);
 
