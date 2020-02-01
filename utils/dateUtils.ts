6b464b146fcda9179065ae65cfd2ef52f92bc27b
Brian David <beejunk@gmail.com>
2020-02-01 10:04:16 -0600

    Update date utils

--- utils/dateUtils.ts
+++ utils/dateUtils.ts
@@ -13,50 +13,66 @@ const MONTHS = [
   'Dec.',
 ];
 
-const getLocalISOFromTimestamp = (timestamp: number): string => {
+function getLocalISOFromTimestamp(timestamp: number): string {
   const date = new Date(timestamp);
   const offset = date.getTimezoneOffset() * 60 * 1000;
   const localDate = new Date(timestamp - offset);
 
   return localDate.toISOString().split('.')[0];
-};
+}
 
-const replaceISOSegment = (timestamp: number, segmentIdx: 0 | 1, value: string): number => {
+function replaceISOSegment(timestamp: number, segmentIdx: 0 | 1, value: string): number {
   const isoSegments = getLocalISOFromTimestamp(timestamp).split('T');
 
   isoSegments[segmentIdx] = value;
 
   return new Date(isoSegments.join('T')).getTime();
-};
+}
 
-export const getMonthStr = (monthIdx: number): string => MONTHS[monthIdx];
+export function getMonthStr(monthIdx: number): string {
+  return MONTHS[monthIdx];
+}
 
-export const appendSuffixToDate = (date: number): string => (
-  String(date).endsWith('2')
-    ? `${date}nd`
-    : `${date}th`
-);
+export function appendSuffixToDate(date: number): string {
+  const dateStr = String(date);
+  const lastNbr = dateStr.slice(-1);
 
-export const updateDate = (timestamp: number, dateISO: string): number => (
-  replaceISOSegment(timestamp, 0, dateISO)
-);
+  if (date > 3 && date < 20) {
+    return `${dateStr}th`;
+  }
 
-export const updateTime = (timestamp: number, timeISO: string): number => (
-  replaceISOSegment(timestamp, 1, timeISO)
-);
+  switch (lastNbr) {
+    case '1':
+      return `${dateStr}st`;
+    case '2':
+      return `${dateStr}nd`;
+    case '3':
+      return `${dateStr}rd`;
+    default:
+      return `${dateStr}th`;
+  }
+}
 
-export const getLocalDate = (timestamp: number): string => (
-  getLocalISOFromTimestamp(timestamp).split('T')[0]
-);
+export function updateDate(timestamp: number, dateISO: string): number {
+  return replaceISOSegment(timestamp, 0, dateISO);
+}
 
-export const getLocalTime = (timestamp: number): string => (
-  getLocalISOFromTimestamp(timestamp).split('T')[1]
-);
+export function updateTime(timestamp: number, timeISO: string): number {
+  return replaceISOSegment(timestamp, 1, timeISO);
+}
 
-export const getFormattedTime = (date: Date): string => {
+export function getLocalDate(timestamp: number): string {
+  return getLocalISOFromTimestamp(timestamp).split('T')[0];
+}
+
+export function getLocalTime(timestamp: number): string {
+  return getLocalISOFromTimestamp(timestamp).split('T')[1];
+}
+
+export function getFormattedTime(date: Date): string {
   const hours = date.getHours() % 12;
   const period = date.getHours() < 12 ? 'am' : 'pm';
   const minutes = date.getMinutes();
 
   return `${hours}:${minutes} ${period}`;
-};
+}
