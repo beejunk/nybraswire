9cb563e1b0ad4df64305ba491e0e7bf103bb2f64
Brian David <beejunk@gmail.com>
2020-02-22 09:33:16 -0600

    Add GitHub/Twitter links

--- components/shared/Layout/layout/Navigation.tsx
+++ components/shared/Layout/layout/Navigation.tsx
@@ -13,13 +13,13 @@ import {
 import firebase from '../../../../firebase';
 import useAuth from '../../../../hooks/useAuth';
 
-const logout = (): void => {
+function logout(): void {
   firebase.auth().signOut();
-};
+}
 
-const Navigation: React.FC = () => {
-  const user = useAuth();
-  const [open, setMenuState] = useState(false);
+const Navigation: React.FC = function Navigation() {
+  let user = useAuth();
+  let [open, setMenuState] = useState(false);
 
   return (
     <Navbar
@@ -30,9 +30,10 @@ const Navigation: React.FC = () => {
       className="mb-5"
     >
       <NavbarBrand
+        className="flex-grow-1"
         tag={(props): React.ReactElement => (
           <Link href="/">
-            <a {...props}>
+            <a className={props.className}>
               Nybraswire ::
               {' '}
               <small className="text-info">
@@ -43,17 +44,52 @@ const Navigation: React.FC = () => {
         )}
       />
 
+      {!user && (
+        <>
+          <a
+            target="_blank"
+            href="https://github.com/beejunk/nybraswire"
+            rel="noopener noreferrer"
+            css={{
+              width: '1.5rem',
+            }}
+          >
+            <img
+              className="img-fluid"
+              src="/assets/images/github_logo.png"
+              alt="GitHub Logo"
+            />
+          </a>
+
+          <a
+            target="_blank"
+            className="ml-2 mr-2"
+            href="https://twitter.com/NybrasWire"
+            rel="noopener noreferrer"
+            css={{
+              width: '1.5rem',
+            }}
+          >
+            <img
+              className="img-fluid"
+              src="/assets/images/twitter_logo.png"
+              alt="Twitter Logo"
+            />
+          </a>
+        </>
+      )}
+
       {user && (
         <>
           <NavbarToggler onClick={(): void => { setMenuState(!open); }} />
 
-          <Collapse isOpen={open} navbar>
+          <Collapse isOpen={open} navbar className="flex-grow-0">
             <Nav className="ml-auto" navbar>
               <NavItem className="mr-3">
                 <NavLink
                   tag={(props): React.ReactElement => (
                     <Link as="/posts/create" href="/posts?create=true">
-                      <a {...props}>
+                      <a className={props.className}>
                         Create Post
                       </a>
                     </Link>
